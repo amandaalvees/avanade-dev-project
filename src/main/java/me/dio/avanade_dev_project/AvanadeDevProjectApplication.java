d3cf87dd8dfa0bee86a30631a61e004feec2a39b
Amanda <amandaleeriver@hotmail.com>
2025-03-11 14:46:06 -0300

    Initial commit

---
+++ src/main/java/me/dio/avanade_dev_project/AvanadeDevProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_dev_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDevProjectApplication.class, args);
+	}
+
+}
